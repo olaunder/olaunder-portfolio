45e195c6080df8bbc8f4987954a05fe9a8ff0104
olaunder <olaunder@gmail.com>
2023-04-11 14:43:49 +1000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,17 +3,25 @@ export const projects = [
     title: "Pokedex App",
     subtitle: "React Hooks & API",
     description:
-      "Created a pokedex app using React",
+      "Created a pokedex app using React to practice using React Hooks and reading and displaying data from an API.",
     image: "./Pokedex.jpg",
     link: "https://olaunder-pokedex-app.netlify.app/",
   },
   {
-    title: "Pokedex App",
-    subtitle: "React Hooks & API",
+    title: "Tic-Tac-Toe",
+    subtitle: "Creating a Tic-Tac-Toe Website",
     description:
-      "Created a pokedex app using React",
-    image: "./Pokedex.jpg",
-    link: "https://olaunder-pokedex-app.netlify.app/",
+      "Created a Tic-Tac-Toe Website using HTML, CSS and JavaScript. This game features a basic AI that the user can play against.",
+    image: "./tictactoe.jpg",
+    link: "https://olaundertictactoe.netlify.app/",
+  },
+  {
+    title: "Rapid Rentals",
+    subtitle: "Creating a Car Rental Website",
+    description:
+      "Created a car rental website using PHP, HTML, CSS, JavaScript and SQL. This website was created for the unit ICTWEB513 for my Diploma of IT. Functionalities of the website include; user registration, user login, listing rentals from a database, checking if rentals are available, rating and return vehicle.",
+    image: "./rapid.jpg",
+    link: "https://olaunder.x10.mx/index.php",
   },
 ];
 
@@ -24,6 +32,6 @@ export const skills = [
   "PHP",
   "SQL",
   "React",
-  "Kotlin",
+  "XML",
   "Netlify"
 ];
